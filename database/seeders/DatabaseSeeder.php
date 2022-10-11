1896cd2402bf9b42511851363b627da20b2590a7
iamrahul1973 <rahulkr@gmail.com>
2022-10-11 20:28:39 +0530

    INITIAL - AUTH, SPATIE ROLE

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        $admin = User::factory()->create(['email' => 'admin@limenzy.in']);
+        $admin->assignRole('admin');
+    }
+}
